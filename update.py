2e460c85de4a110774d010ba1ff1cea2e4e79aad
Pramuditha Vidushan <75202685+eXtizi@users.noreply.github.com>
2022-10-05 09:51:21 +0530

    Update update.py

--- update.py
+++ update.py
@@ -37,7 +37,7 @@ try:
     if len(UPSTREAM_REPO) == 0:
        raise TypeError
 except:
-    UPSTREAM_REPO = "https://github.com/eXtizi/anastymirror"
+    UPSTREAM_REPO = "https://github.com/extizi/anastymirror"
 try:
     if len(UPSTREAM_BRANCH) == 0:
        raise TypeError
